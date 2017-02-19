391b38fbcce1be083a64853e6ca158acacf55e20
Samuel <sammohr97@gmail.com>
2017-02-18 23:12:57 -0500

    Version 1.4

---
+++ app/src/main/java/com/cs2340/smores/m4/controllers/EditAccountActivity.java
@@ -0,0 +1,22 @@
+package com.cs2340.smores.m4.controllers;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.cs2340.smores.m4.R;
+
+/**
+ * Created by smores on 2/18/17.
+ */
+
+public class EditAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_account_activity);
+
+
+    }
+}
